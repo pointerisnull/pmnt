3485a3d719494123130aba7a46173148c2a5c9ce
Bren <92599085+pointerisnull@users.noreply.github.com>
2026-03-30 12:23:01 -0400

    Fixed CRC16 calculation

--- src/core/crypto.c
+++ src/core/crypto.c
@@ -3,6 +3,7 @@
 #include <stddef.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 hash64_t str_to_bin(char *str) {
 	hash64_t code = (hash64_t) strtol(str, NULL, 16);
@@ -10,23 +11,19 @@ hash64_t str_to_bin(char *str) {
 }
 
 crc16_t crc16(byte_t *data, size_t length) {
-	const crc16_t POLY = 0x8005;
-	const crc16_t INITIAL_VALUE = 0xFFFF;
+	crc16_t crc = 0xFFFF;
 
-    crc16_t crc = INITIAL_VALUE;
-    for (size_t i = 0; i < length; ++i) {
-        uint8_t index = (uint8_t)(crc ^ data[i]);
-        crc = (uint16_t)((crc >> 8) ^ crc_table[index]);
+    for (size_t i = 0; i < length; i++) {
+        int table_index = (byte_t)(crc ^ data[i]);
+        crc = (crc >> 8) ^ crc_table[table_index];
     }
+
     return crc;
 }
 
-crc16_t validate_crc(byte_t *data, size_t length, crc16_t crc) {
-	const crc16_t SHIFTMASK = 0x00FF;
-
-    for(int i = 0; i < length; i++) {
-        crc16_t table_value = crc_table[((crc >> 8) ^ *(byte_t*)data++) & SHIFTMASK];
-        crc = (crc << 8) ^ table_value;
-    }
-	return crc;
+bool validate_crc(byte_t *data, size_t length, crc16_t crc) {
+    crc16_t data_crc = crc16(data, length);
+    if (crc == data_crc)
+        return true;
+    return false;
 }
--- src/main.c
+++ src/main.c
@@ -6,8 +6,16 @@
 int main(int argc, char **argv) {
 	if (argc > 1) {
 		size_t len = strlen(argv[1]);
-		printf("String Input: %s\n", argv[1]);
-		printf("CRC16: %x\n", crc16((byte_t *) argv[1], len));
+		printf("String Input: %s\tBytes: %d\n", argv[1], len);
+		crc16_t crc = crc16((byte_t *) argv[1], len);
+		printf("CRC16: %x\n", crc);
+
+
+		*argv[1] = 0xFF; // Corrupt a byte
+		if (validate_crc((byte_t *)argv[1], len, crc))
+			printf("CRC Validated!\n");
+		else
+			printf("CRC values do not match, data must be corrupt!\n");
 	}
 
 
